164f873793106f47e0b068602ddfef4e736addf5
Giacomo Marinangeli <giacomo@cloudacademy.com>
2016-10-17 19:05:13 +0200

    Input validation with JWT

--- main.go
+++ main.go
@@ -13,16 +13,19 @@ import (
 
 	zipper "github.com/cloudacademy/s3zipper/core"
 	"github.com/cloudacademy/s3zipper/s3"
+	"github.com/dgrijalva/jwt-go"
 )
 
 var s3bucket *s3.S3Bucket
 
 var configFile string
+var conf *Configuration
 
 type Configuration struct {
-	Bucket string
-	Region string
-	Port   int
+	Bucket       string
+	Region       string
+	Port         int
+	JWTSharedKey string
 }
 
 func init() {
@@ -37,16 +40,16 @@ func main() {
 
 	decoder := json.NewDecoder(configJSON)
 
-	c := Configuration{}
-	err = decoder.Decode(&c)
+	conf = &Configuration{}
+	err = decoder.Decode(conf)
 	checkError(err)
 
-	s3bucket, err = s3.New(c.Region, c.Bucket)
+	s3bucket, err = s3.New(conf.Region, conf.Bucket)
 	checkError(err)
 
-	fmt.Println("Running on port", c.Port)
+	fmt.Println("Running on port", conf.Port)
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":"+strconv.Itoa(c.Port), nil)
+	http.ListenAndServe(":"+strconv.Itoa(conf.Port), nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -55,10 +58,15 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	// Get "id" URL params
 	ids, ok := r.URL.Query()["id"]
 	if !ok || len(ids) < 1 {
-		http.Error(w, "S3 File Zipper. Pass ?id= to use.", 400)
+		http.Error(w, "S3 File Zipper. Pass JWT token with ?id=", 400)
 		return
 	}
-	prefix := ids[0]
+	prefix, err := parseJWT(ids[0])
+	if err != nil {
+		log.Printf("Error decoding JWT: %s\n", err)
+		http.Error(w, "Invalid JWT signature", 400)
+		return
+	}
 
 	zipname := prefix
 	if name, ok := r.URL.Query()["name"]; ok && len(name) >= 1 {
@@ -91,4 +99,34 @@ func checkError(err error) {
 	if err != nil {
 		log.Fatal("Error: ", err)
 	}
+}
+
+func parseJWT(tokenString string) (string, error) {
+
+	// Parse takes the token string and a function for looking up the key. The latter is especially
+	// useful if you use multiple keys for your application.  The standard is to use 'kid' in the
+	// head of the token to identify which key to use, but the parsed token (head and claims) is provided
+	// to the callback, providing flexibility.
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return "", fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(conf.JWTSharedKey), nil
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	if !token.Valid {
+		return "", fmt.Errorf("invalid token: %v", token.Raw)
+	}
+
+	id, ok := token.Claims["id"]
+	if !ok {
+		return "", fmt.Errorf("missing `id` property in jwt token")
+	}
+
+	return id.(string), nil
 }
